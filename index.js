3b5acbf818616c789f7d0ef30fa2ef8d2fcf2284
Stephan Samuel <ssamuel@xylie.net>
2018-07-09 08:45:11 -0500

    Initial add of node files.

---
+++ index.js
@@ -0,0 +1,23 @@
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+var app = express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+
+  app.get('/times', (req, res) => {
+	  let result = ''
+	  const times = process.env.TIMES || 5
+console.log(times)
+	  for (i = 0; i < times; i++) {
+		  result += i + ''
+	  }
+	  res.send(result)
+  })
+  
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
